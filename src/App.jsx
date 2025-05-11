f4d697245b70f2fde65661d4421809d7256b494e
chouchou77 <khalfawi331@gmail.com>
2025-05-11 13:28:02 +0100

    envirenment

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
+
+function App() {
+  return (
+    <>
+      <h1>advence</h1>
+      <FontAwesomeIcon icon={faBars} />
+    </>
+  )
+}
+
+export default App
